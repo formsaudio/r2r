b28ca97ead8b5821175f43cba3a54710f4582cd2
Birm <birm@rbirm.us>
2018-01-14 23:36:18 -0500

    basic server

--- r2r.js
+++ r2r.js
@@ -1,24 +1,57 @@
-var xp = require('express');
-var mg = require('mongodb');
+const xp = require('express');
+const mg = require('mongodb').MongoClient;
+const helmet = require('helmet');
 
-var r2r = xp();
+const mongo_url = process.env.MONGOURL || "mongodb://localhost:27017";
 
-var login = function(req, res){
+
+const r2r = xp();
+
+r2r.use(helmet())
+
+var login = function(req, res) {
     res.status(501);
 }
 
-var new_key = function(req, res){
+var new_key = function(req, res) {
     // TODO better security
     res.status(501);
 }
 
 
-var get_program = function(req, res){
-    res.status(501);
+var get_program = function(req, res) {
+    mg.connect(mongo_url, function(err, client) {
+            assert.equal(null, err);
+            const db = client.db("FA_r2r");
+            // TODO validate before returning
+            db.collection("programs").findOne({
+                    name: req.body.name
+                },
+                function(err, result) {
+                    if (err) {
+                        res.status(500);
+                    } else {
+                        res.send(result)
+                    };
+                    db.close();
+                });)
+    });
 }
 
-var save_program = function (req, res){
-    res.status(501);
+var save_program = function(req, res) {
+    mg.connect(mongo_url, function(err, client) {
+            assert.equal(null, err);
+            const db = client.db("FA_r2r");
+            // TODO validate before adding
+            db.collection("programs").insertOne(req.body.program, function(err, result) {
+                if (err) {
+                    res.status(500);
+                } else {
+                    res.send(result)
+                };
+                db.close();
+            });)
+    });
 }
 
 r2r.get("/get", get_program);
